c603d7230d52625cb9f8af2af5ccea70fb2d6297
yangbo <yangbo@superred.com.cn>
2022-05-04 19:22:32 +0800

    随机图片

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">亲爱的朱玲莉，欢迎回来！</div>
+    <el-image id="dashboard" class="dashboard-image" :src="this.imgSrc"  />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+import axios from 'axios'
 export default {
   name: 'Dashboard',
   computed: {
     ...mapGetters([
       'name'
     ])
+  },
+  data() {
+    return {
+      imgSrc: '',
+      cat: ''
+    }
+  },
+  mounted() {
+    axios.get('https://api.thecatapi.com/v1/images/search').then(res => {
+      console.log(res.data[0].url)
+      this.imgSrc = res.data[0].url
+    })
   }
 }
+
 </script>
 
 <style lang="scss" scoped>
